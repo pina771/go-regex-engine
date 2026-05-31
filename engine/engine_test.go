6044f230b485a72676f26129c638e6ef9eb41b3f
pina771 <filip.katarina@gmail.com>
2026-05-31 22:01:49 +0200

    reformat engine test

--- engine/engine_test.go
+++ engine/engine_test.go
@@ -18,10 +18,13 @@ func TestMatch(t *testing.T) {
 		{"xtaaab", "xta*b"},
 		{"aaaabbcc", "a*b*c*d*"},
 	}
-	for _, input := range inputs {
-		if matches := Match(input.text, input.regex); !matches {
-			t.Fatalf("Input %s should match regex %s. Got=%v",
-				input.text, input.regex, matches)
+	for i := range inputs {
+		if matches := Match(inputs[i].text, inputs[i].regex); !matches {
+			t.Fatalf("fail[%d]. String:%s\tRegex:%s",
+				i,
+				inputs[i].text,
+				inputs[i].regex,
+			)
 		}
 
 	}
